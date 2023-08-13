2e226ab4d89de3c9f845a21aaddf26742643090a
Madiyar Kenzhebayev <madikensky2003@gmail.com>
2023-08-14 00:14:24 +0600

    feat: finish project

--- library/index.js
+++ library/index.js
@@ -1,1 +1,27 @@
-console.log("1)Вёрстка валидная 10/10\n2)Вёрстка семантическая 16/16\n3)Вёрстка соответствует макету 54/54\n4)Общие требования к верстке 20/20")
+// document.querySelector(".burger").addEventListener("click", function () {
+//   this.classList.toggle("active");
+//   document.querySelector(".nav-list").classList.toggle("show-nav-list");
+// });
+const burgerIcon = document.querySelector('.burger');
+const navList = document.querySelector('.nav-list');
+const menuLinks = document.querySelectorAll('.nav-list a');
+
+burgerIcon.addEventListener('click', () => {
+  burgerIcon.classList.toggle('active');
+  navList.classList.toggle('show-nav-list');
+});
+
+document.addEventListener('click', (event) => {
+  if (!navList.contains(event.target) && !burgerIcon.contains(event.target)) {
+    burgerIcon.classList.remove('active');
+    navList.classList.remove('show-nav-list');
+  }
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navList.classList.remove('show-nav-list');
+    burgerIcon.classList.remove('active');
+  });
+});
+
